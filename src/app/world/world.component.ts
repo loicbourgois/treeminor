2b885490ab7cd59e82b558732da2f6b78859adf6
loicbourgois <loic@bourgois.pro>
2019-03-01 22:13:04 +0100

    Setup Wolrd WebGL up to single triangle rendering

--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -39,6 +39,43 @@ export class WorldComponent implements OnInit {
         this.vertexShader = this.createShader(gl, gl.VERTEX_SHADER, this.vertexShaderSource);
         this.fragmentShader = this.createShader(gl, gl.FRAGMENT_SHADER, this.fragmentShaderSource);
         let program = this.createProgram(gl, this.vertexShader, this.fragmentShader);
+        
+        // Attributes
+        var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+        // Buffer
+        var positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        var positions = [
+          0, 0,
+          0, 0.5,
+          0.7, 0,
+        ];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+        // Vertex Array
+        var vao = gl.createVertexArray();
+        gl.bindVertexArray(vao);
+        gl.enableVertexAttribArray(positionAttributeLocation);
+        var size = 2;          // 2 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            positionAttributeLocation, size, type, normalize, stride, offset)
+        // Resizing
+        this.resizeCanvasToDisplaySize(gl.canvas);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        // Clear the canvas
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        //
+        gl.useProgram(program);
+        gl.bindVertexArray(vao);
+        //
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 3;
+        gl.drawArrays(primitiveType, offset, count);
       },
       error => console.error(error)
     );
@@ -67,6 +104,21 @@ export class WorldComponent implements OnInit {
     }
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
+  }
+  
+  resizeCanvasToDisplaySize(canvas) {
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+   
+    // Check if the canvas is not the same size.
+    if (canvas.width  !== displayWidth ||
+        canvas.height !== displayHeight) {
+   
+      // Make the canvas the same size
+      canvas.width  = displayWidth;
+      canvas.height = displayHeight;
+    }
   }
 
 }

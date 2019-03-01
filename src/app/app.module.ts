ecce7e312552ef96b9483a5c128382c08215a679
loicbourgois <loic@bourgois.pro>
2019-03-01 17:37:40 +0100

    Add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

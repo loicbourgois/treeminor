31bbdd272065a00e8bb66fa919d94104562f2c8b
loicbourgois <loic@bourgois.pro>
2019-03-05 18:24:27 +0100

    Lint and correct

--- src/app/engine/renderer.ts
+++ src/app/engine/renderer.ts
@@ -11,17 +11,18 @@ export class Renderer {
     this.fragmentShaderSource = fragmentShaderSource;
     this.vertexShaderSource = vertexShaderSource;
     const gl = canvas.getContext('webgl2');
-    if(!gl) {
+    if (!gl) {
+      console.error('No WebGL for you');
     }
 
     this.vertexShader = this.createShader(gl, gl.VERTEX_SHADER, this.vertexShaderSource);
     this.fragmentShader = this.createShader(gl, gl.FRAGMENT_SHADER, this.fragmentShaderSource);
-    let program = this.createProgram(gl, this.vertexShader, this.fragmentShader);
+    const program = this.createProgram(gl, this.vertexShader, this.fragmentShader);
 
     // Attributes
-    let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
     // Buffer
-    let positionBuffer = gl.createBuffer();
+    const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     const positions = [
       0, 0,
@@ -30,16 +31,16 @@ export class Renderer {
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
     // Vertex Array
-    let vao = gl.createVertexArray();
+    const vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
     gl.enableVertexAttribArray(positionAttributeLocation);
-    let size = 2;          // 2 components per iteration
-    let type = gl.FLOAT;   // the data is 32bit floats
-    let normalize = false; // don't normalize the data
-    let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    let offset = 0;        // start at the beginning of the buffer
+    const size = 2;          // 2 components per iteration
+    const type = gl.FLOAT;   // the data is 32bit floats
+    const normalize = false; // don't normalize the data
+    const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    const offset = 0;        // start at the beginning of the buffer
     gl.vertexAttribPointer(
-        positionAttributeLocation, size, type, normalize, stride, offset)
+        positionAttributeLocation, size, type, normalize, stride, offset);
     // Resizing
     this.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -50,17 +51,17 @@ export class Renderer {
     gl.useProgram(program);
     gl.bindVertexArray(vao);
     //
-    let primitiveType = gl.TRIANGLES;
-    let offset = 0;
-    let count = 3;
+    const primitiveType = gl.TRIANGLES;
+    const offset = 0;
+    const count = 3;
     gl.drawArrays(primitiveType, offset, count);
   }
 
   createShader(gl, type, source) {
-    let shader = gl.createShader(type);
+    const shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
-    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
     if (success) {
       return shader;
     }
@@ -69,11 +70,11 @@ export class Renderer {
   }
 
   createProgram(gl, vertexShader, fragmentShader) {
-    let program = gl.createProgram();
+    const program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
-    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
     if (success) {
       return program;
     }
@@ -83,8 +84,8 @@ export class Renderer {
 
   resizeCanvasToDisplaySize(canvas) {
     // Lookup the size the browser is displaying the canvas.
-    let displayWidth  = canvas.clientWidth;
-    let displayHeight = canvas.clientHeight;
+    const displayWidth  = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
     // Check if the canvas is not the same size.
     if (canvas.width  !== displayWidth ||
         canvas.height !== displayHeight) {
--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { DownloaderService } from '../services/downloader.service';
 import { forkJoin } from 'rxjs';
 import { Renderer } from '../engine/renderer';
@@ -9,7 +9,7 @@ import { World } from '../engine/world';
   templateUrl: './world.component.html',
   styleUrls: ['./world.component.css']
 })
-export class WorldComponent implements OnInit {
+export class WorldComponent implements AfterViewInit {
 
   fragmentShaderUrl = 'assets/shaders/world.frag';
   vertexShaderUrl = 'assets/shaders/world.vert';
@@ -25,18 +25,15 @@ export class WorldComponent implements OnInit {
   constructor(private downloaderService: DownloaderService) {
   }
 
-  ngOnInit() {
-  }
-
   ngAfterViewInit() {
     forkJoin(
       this.downloaderService.getTextFile(this.fragmentShaderUrl),
       this.downloaderService.getTextFile(this.vertexShaderUrl)
     ).subscribe(
       data => {
-        let fragmentShaderSource = data[0];
-        let vertexShaderSource = data[1];
-        let canvas : any = document.getElementById('canvas');
+        const fragmentShaderSource = data[0];
+        const vertexShaderSource = data[1];
+        const canvas: any = document.getElementById('canvas');
         this.world = new World(10, 10);
         this.renderer = new Renderer(canvas, fragmentShaderSource, vertexShaderSource);
       },
